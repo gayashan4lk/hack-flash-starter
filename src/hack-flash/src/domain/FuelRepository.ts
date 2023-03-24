43df1a79bc7cde63e3a07a74123a1361b7e55a51
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-03-24 12:35:45 +0530

    add insert fuel request

--- src/hack-flash/src/domain/FuelRepository.ts
+++ src/hack-flash/src/domain/FuelRepository.ts
@@ -2,6 +2,10 @@ import { FuelRequest } from './FuelRequest';
 import { deleteAllRequests } from './FirebaseApp';
 
 export class FuelRepository {
+	async insert(fuelRequest: FuelRequest): Promise<boolean> {
+		return true;
+	}
+
 	async delete(): Promise<boolean> {
 		deleteAllRequests();
 		return true;
